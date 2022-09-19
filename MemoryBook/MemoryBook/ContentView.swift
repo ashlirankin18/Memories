4d167287ce1f18d59c8f7cc38877eac683015b3f
Ashli Rankin <ashlirankin18@gmail.com>
2022-09-19 11:40:09 -0400

    Initial Commit

---
+++ MemoryBook/MemoryBook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MemoryBook
+//
+//  Created by chief on 9/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MemoryBook/MemoryBook/MemoryBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryBookApp.swift
+//  MemoryBook
+//
+//  Created by chief on 9/19/22.
+//
+
+import SwiftUI
+
+@main
+struct MemoryBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
